a60ebb3029c727c6a10d67d269ad083736a1d8e9
Omkar Mohite <109783366+OmkarMohite505@users.noreply.github.com>
2023-02-16 17:30:32 +0530

    tested

--- src/app/service/apiservice.service.ts
+++ src/app/service/apiservice.service.ts
@@ -22,33 +22,41 @@ export class ApiserviceService {
     return this.httpService.get(`http://localhost:3000/posts?_sort=${column}&_order=asc`)
   }
   updateStatus(id: number, data: any) {
-    return this.httpService.put(`http://localhost:3000/posts/` + id, data)
-      .subscribe({
-        next: (res) => {
-          // Updating object 
-          let ob = {
-             category: res.category,
-             department: res.department,
-             division: res.division,
-             endDate: res.endDate,
-             location: res.location,
-             priority: res.priority,
-             projectName: res.projectName,
-             reason: res.reason,
-             startDate: res.startDate,
-             status: obj.status,
-             type: res.type
-          }
-          
-          
-            this.httpService.post('http://localhost:3000/posts/'+id, ob); // adding updated object in JSON server
-          console.log(res)
-          alert("status updated successfully")
-        },
-        error: () => {
-          alert("status update error")
-        }
-      })
+    return this.httpService.get(`http://localhost:3000/posts/` + id).subscribe({
+      next: (res: any) => {
+        // Updating object
+        let ob = {
+          category: res.category,
+          department: res.department,
+          division: res.division,
+          endDate: res.endDate,
+          location: res.location,
+          priority: res.priority,
+          projectName: res.projectName,
+          reason: res.reason,
+          startDate: res.startDate,
+          status: data.status,
+          type: res.type,
+        };
+        console.log(data.status);
+        console.log(ob);
+        this.httpService
+          .put('http://localhost:3000/posts/' + id, ob)
+          .subscribe({
+            next: (data: any) => {
+              console.log(data);
+            },
+            error: (data: any) => {
+              console.log(data);
+            },
+          }); // adding updated object in JSON server
+
+        alert('status updated successfully');
+      },
+      error: () => {
+        alert('status update error');
+      },
+    });
   }
 }
 
